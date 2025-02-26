3ca0c500f6cf2a42a1130309989c5d989c41326e
furkankalkan <furkankalkan@mantis.com.tr>
2025-02-26 18:11:56 +0300

    WIP: refactored distribute-js-translations command to better readability and maintainability

--- invenio_i18n/cli.py
+++ invenio_i18n/cli.py
@@ -14,6 +14,7 @@ from importlib.metadata import entry_points
 from pathlib import Path
 
 from flask.cli import with_appcontext
+from flask import current_app
 
 
 @click.group(chain=True)
@@ -24,56 +25,75 @@ def i18n():
 
 @i18n.command()
 @with_appcontext
-@click.option('-i', '--input-directory',
-              required=True,
-              type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=False, path_type=Path),
-              help='Input directory containing translations in JSON format.')
-def distribute_js_translations(input_directory):
-    """Distribute JavaScript translations"""
-    EXCEPTIONAL_PACKAGE_NAME_MAPPER = {
-        "jobs": "invenio-jobs",
-        "invenio-previewer-theme": "invenio-previewer",
-        "invenio-app-rdm-theme": "invenio-app-rdm"
+@click.option(
+    "-i",
+    "--input-directory",
+    required=True,
+    type=click.Path(
+        exists=True, file_okay=False, dir_okay=True, writable=False, path_type=Path
+    ),
+    help="Input directory containing translations in JSON format.",
+)
+@click.option(
+    "--entrypoint-group",
+    default="invenio_assets.webpack",
+    help="Entrypoint group used to get package assets paths. Default: \"invenio_assets.webpack\" You don't need to set this option under normal circumstances.'",
+)
+def distribute_js_translations(input_directory: Path, entrypoint_group: str):
+    """Distribute JS/React localizations"""
+
+    # TODO: should we keep naming "translation" or change to "localization"?
+
+    exceptional_package_name_mapper = current_app.config[
+        "I18N_JS_DISTR_EXCEPTIONAL_PACKAGE_MAP"
+    ]
+    bundle_entrypoints = [ep for ep in entry_points(group=entrypoint_group)]
+
+    # Map package names to their translation file base paths for packages with a webpack entrypoint.
+    package_translations_base_paths = {
+        exceptional_package_name_mapper.get(
+            bundle_entrypoint.name, bundle_entrypoint.name
+        ): Path(bundle_entrypoint.load().path)
+        / "translations"
+        for bundle_entrypoint in bundle_entrypoints
+        if bundle_entrypoint.load().path
     }
-    bundle_entrypoints = [ep for ep in entry_points(group="invenio_assets.webpack")]
 
-    PACKAGE_TRANSLATIONS_BASE_PATHS = {}
-    for bundle_entrypoint in bundle_entrypoints:
-        package_name = bundle_entrypoint.name.replace("_", "-")
-        bundle = bundle_entrypoint.load()
-        if bundle.path:
-            if package_name in EXCEPTIONAL_PACKAGE_NAME_MAPPER:
-                package_name = EXCEPTIONAL_PACKAGE_NAME_MAPPER[package_name]
-            PACKAGE_TRANSLATIONS_BASE_PATHS[package_name] = Path(bundle.path) / "translations"
+    # Map source translation file paths to their languages
+    source_translation_files = {
+        path.stem: path
+        for path in input_directory.iterdir()
+        if path.is_file() and path.suffix == ".json"
+    }
 
-    # Get combined translation files by languages
-    source_translation_files = {}
-    for path in input_directory.iterdir():
-        if path.is_file() and path.suffix == ".json":
-            source_translation_files[path.stem] = path
+    # Read unified source translation files and distribute translations to relevant packages
+    for language, source_file_path in source_translation_files.items():
+        with source_file_path.open("r") as source_file:
+            unified_translations = json.load(source_file)
 
-    for lang in source_translation_files:
-        with open(source_translation_files[lang], 'r') as source_translations_file:
-            combined_translations = source_translations_file.read()
-            combined_translations = json.loads(combined_translations)
-            for package in combined_translations:
-                if package in PACKAGE_TRANSLATIONS_BASE_PATHS:
-                    module = package.replace("-", "_")
-                    target_translations_path = PACKAGE_TRANSLATIONS_BASE_PATHS[package] / module / "messages" / lang
-                    if not Path(target_translations_path).exists():
-                        click.secho(
-                            f"Translation directory for {lang} not found in {PACKAGE_TRANSLATIONS_BASE_PATHS[package] / module / "messages"}. Creating...",
-                            fg="yellow")
-                        Path(target_translations_path).mkdir()
+        for package_name, translations in unified_translations.items():
+            base_path = package_translations_base_paths.get(
+                package_name,
+            )
+            if not base_path:
+                click.secho(
+                    f"Package {package_name} doesn't have webpack entrypoint. Skipping...",
+                    fg="yellow",
+                    italic=True,
+                )
+                continue
 
-                    target_translations_file_path = Path(target_translations_path) / "translations.json"
-                    with open(target_translations_file_path, "w+") as target_translations_file:
-                        try:
-                            target_translations_file.write(
-                                json.dumps(combined_translations[package], indent=2, ensure_ascii=False))
-                            click.secho(f"Translations for {package} in {lang} have been written.", fg="green")
-                        except Exception as e:
-                            raise e
+            target_translations_path = base_path / package_name / "messages" / language
+            # TODO: maybe we should add confirmation or info message for missing languages before create their directories?
+            # TODO: set parents=False if base_path / domain / "messages" part is mandatory. Custom 3rd party packages might not have this, idk.
+            target_translations_path.mkdir(parents=True, exist_ok=True)
 
-                else:
-                    click.secho(f"{package} doesn't have webpack entrypoint. Skipping...", fg="yellow", italic=True)
+            with (target_translations_path / "translations.json").open(
+                "w"
+            ) as target_file:
+                json.dump(translations, target_file, indent=2, ensure_ascii=False)
+
+            click.secho(
+                f"{package_name} localizations for language {language} have been written.",
+                fg="green",
+            )
